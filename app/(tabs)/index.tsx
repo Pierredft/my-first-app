5775a36c3744cf08440171a13a4fe2ab70390d2a
pierre defauquet <pierredefauquet@gmail.com>
2025-08-30 13:49:18 +0200

    fin de la foc 4

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,15 @@
-import { Text } from "react-native";
+import Human from "@/components/Human/Human";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
 
 export default function HomeScreen() {
   return (
-    <Text>Hello</Text>
+    <ThemedView style={{ flex: 1, justifyContent: "center", alignItems: "center"}} >
+      <Human />
+      <Human />
+      <ThemedText>
+        Fin de l écran
+      </ThemedText>
+    </ThemedView>
   );
 }
