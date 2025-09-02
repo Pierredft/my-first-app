576c0da6515bfd63a1942028f3a4410b041a8384
pierre defauquet <pierredefauquet@gmail.com>
2025-09-02 11:02:53 +0200

    fin de la doc 6

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,13 @@
+import { Image } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-import { FlexDemo } from '../../components/FlexDemo';
+import { Human } from '../../components/Human/Human';
 
 export default function HomeScreen() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1 }}>
-        <FlexDemo />
+      <SafeAreaView>
+        <Human firstName="pierre" name="DEFAUQUET" age={28} />
+        <Image style={{height: 200, width: 200}} source={{uri : "https://www.assuropoil.fr/wp-content/uploads/berger-australien.jpg"}} />
       </SafeAreaView>
     </SafeAreaProvider>
   );
